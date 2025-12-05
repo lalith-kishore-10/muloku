1d93baef45e0a35554e8d4a3e40dbc3b25f48dc7
lalith-kishore-10 <lalithkishore.447.shs@gmail.com>
2025-12-05 21:59:11 +0530

    Small Patch

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || "https://5cd0f4f9514f.ngrok-free.app";
+const SOCKET_URL =
+  import.meta.env.VITE_SOCKET_URL || "https://5cd0f4f9514f.ngrok-free.app";
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false,
